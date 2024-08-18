11375352b3ae947aea375cfab2799f415cf64d95
YFSCIbrahimYildiz <yldzibrhim@gmail.com>
2024-08-18 13:33:22 +0300

    Creat

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+//ilk ders//
+//değişkenler
+
+console.log(7000+(7000*0.3))//+%30 zam
+console.log(6000+(6000*0.3))//değişkensiz manuel hesap
+
+
+var maasA=7000;
+var maasC=6000;
+console.log(maasA+(maasA*0.3));
+console.log(maasC+(maasC*0.3));
+
+
+var zam=0.3;
+console.log(maasA+(maasA*zam));
+console.log(maasC*(maasC*0.3));
