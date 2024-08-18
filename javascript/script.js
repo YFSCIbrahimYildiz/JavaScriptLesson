0b4dac648431085996b01626fae17f15b68467c3
YFSCIbrahimYildiz <yldzibrhim@gmail.com>
2024-08-18 17:11:56 +0300

    update script

--- javascript/script.js
+++ javascript/script.js
@@ -13,4 +13,33 @@ console.log(maasC+(maasC*0.3));
 
 var zam=0.3;
 console.log(maasA+(maasA*zam));
-console.log(maasC*(maasC*0.3));
+console.log(maasC+(maasC*0.3));
+//değişken türleri
+
+var productName="Iphone 13";
+let productSell="15000";
+
+console.log(typeof productName);
+console.log(typeof productSell);
+
+let num1="10";
+let num2="20";
+
+console.log(Number(num1)+Number(num2));
+
+let num3=10;
+let num4=20;
+console.log(num3.toString()+num4.toString());
+
+let name="Seçil";
+let surname="yazgı";
+
+console.log(name+" "+surname);
+let expoint = 40;
+let successfull =(expoint>=50);
+console.log(successfull);
+console.log(typeof successfull);
+
+let age;
+console.log(age);
+console.log(typeof age);
