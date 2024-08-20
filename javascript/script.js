2200fc05ec7d2aa7181e752657216943e6ddd01c
YFSCIbrahimYildiz <yldzibrhim@gmail.com>
2024-08-20 23:35:58 +0300

    update

--- javascript/script.js
+++ javascript/script.js
@@ -88,7 +88,7 @@ console.log(statu2);
 5)url ve kursAdi değişkenlerini kullanarak aşağıdaki string metni oluşturunuz.
     a)https://www.scarlettrose.com.tr/komple-web-geliştirme-kursu
 */
-let url="https://www.scarlettrose.com.tr/";
+/*let url="https://www.scarlettrose.com.tr/";
 let kursAdi="Komple Web Geliştirme Kursu";
 //1. soru
 let str=url.length;
@@ -113,3 +113,65 @@ let newText1=newText.replaceAll(" ","-");
 //newText.replaceAll(" ","-");
 
 console.log(newText1);
+*/
+//Array
+
+//1)"elma, armut, muz, çilek" elemanlarına sahip dizi oluşturunuz.
+let lArray=["Elma","Armut","Muz","Çilek"];
+
+//2) Dizi kaç elemanlı?
+let length= lArray.length;
+console.log(length);
+
+//3) dizinin ilk ve son elemanı nedir?
+i=0;
+console.log("Dizinin İlk Değeri: "+lArray[0]);
+console.log("Dizinin Son Değeri: "+lArray[length-1]);
+
+
+//4) elma dizinin bir elemanı mıdır?
+let result= lArray.indexOf("Elma")+1;
+if(result==true){
+    console.log("Evet Elma Dizinin bir elemanıdır");
+}
+else{
+    console.log("Elma dizinin bir elemanı değildir. "+result);
+}
+
+//Kiraz Kelimesini listenin sonuna ekleyiniz
+
+lArray.push("Kiraz");
+console.log(lArray);
+//dizinin son 2 elemanını siliniz
+let a=0;
+
+while(a<2){
+
+    lArray.pop();
+    a++;
+}
+console.log(lArray);
+
+//öğrenci bilgilerini saklayınız
+
+let ogr1=["Yiğit","Bilgi",2010,[70,60,80]];
+let ogr2=["Ada","Bilgi",2012, [80,80,90]];
+let ogr3=["Ahmet","Turan",2009,[60,60,70]];
+
+let ogrenciler=[ogr1,ogr2,ogr3];
+let yigityas= new Date().getFullYear()-ogrenciler[0][2];
+
+let adayas=new Date().getFullYear()-ogrenciler[1][2];
+let ahmetyas=new Date().getFullYear()-ogrenciler[2][2];
+console.log(yigityas);
+console.log(adayas);
+console.log(ahmetyas);
+let yigitnot= (ogrenciler[0][3][0]+ogrenciler[0][3][1]+ogrenciler[0][3][2])/3;
+let adanot=(ogrenciler[1][3][0]+ogrenciler[1][3][1]+ogrenciler[1][3][2])/3;
+let ahmetnot=(ogrenciler[2][3][0]+ogrenciler[2][3][1]+ogrenciler[2][3][2])/3;
+
+console.log(yigitnot.toFixed(2));
+console.log(adanot.toFixed(2));
+console.log(ahmetnot.toFixed(2));
+
+
